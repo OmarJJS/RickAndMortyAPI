99faaad42ccc36d7328c1e810512d24d0059351a
Omar Jimenez <jjsomar98@gmail.com>
2024-02-23 20:15:06 -0600

    Deleted Text commented

--- RickAndMortyAPI/Controller/Other/Search/SearchViewController.swift
+++ RickAndMortyAPI/Controller/Other/Search/SearchViewController.swift
@@ -123,15 +123,4 @@ extension SearchViewController: SearchViewDelegate {
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
