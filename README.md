c801412d6605af1fd3ac5debf30858261a9b0a6e
Omar Jimenez <jjsomar98@gmail.com>
2024-02-23 23:50:10 -0600

    Added README

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# RickAndMortyAPI
+
+RickAndMortyAPI is an iOS App created using a REST API consume to show us, the current project show us in the main screen
+
+- Characters: Their status, gender, Type, Specie, Origin, Location, created, Episode Count and episodes where appears
+- Location: Places where appears in the serie
+- Episodes: All Rick and Morty Episodes
+
+This Project is working with
+
+- Written in Swift
+- Uses both UIKit and SwiftUI
+- Designed with MVVM pattern
+- No External Dependencies
+- Available with iOS 17.2 and less
+- Using XCode Version 15.2
+- Swift Language Version 5.9
+
+I made this project taking a good practice of API's consume and giving like a result a nice Rick and Morty App I recommen widely to iOS developers practices like this to fill you of knowledge in Swift UI and have a cute app
+
+The installation process is easy, you can run this app using the XCode IDE in your computer and after download this project of my github project, run and install in your iOS device or in your simulator
+
--- RickAndMortyAPI/Controller/Core/TabViewController.swift
+++ RickAndMortyAPI/Controller/Core/TabViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class TabViewController: UITabBarController {
-//class TabViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,31 +19,27 @@ class TabViewController: UITabBarController {
         let characterVC = CharacterViewController()
         let locationVC = LocationViewController()
         let episodeVC = EpisodeViewController()
-        let settingsVC = SettingsViewController()
         
         characterVC.navigationItem.largeTitleDisplayMode = .automatic
         locationVC.navigationItem.largeTitleDisplayMode = .automatic
         episodeVC.navigationItem.largeTitleDisplayMode = .automatic
-        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
         
         
         let navCharacter = UINavigationController(rootViewController: characterVC)
         let navLocation = UINavigationController(rootViewController: locationVC)
         let navEpisode = UINavigationController(rootViewController: episodeVC)
-        let navSettings = UINavigationController(rootViewController: settingsVC)
         
         navCharacter.tabBarItem = UITabBarItem(title: "Character", image: UIImage(systemName: "person.fill.questionmark"), tag: 1)
         navLocation.tabBarItem = UITabBarItem(title: "Location", image: UIImage(systemName: "location.circle.fill"), tag: 2)
         navEpisode.tabBarItem = UITabBarItem(title: "Episode", image: UIImage(systemName: "movieclapper.fill"), tag: 3)
-        navSettings.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 4)
+
         
-        
-        for nav in [navCharacter,navLocation, navEpisode, navSettings] {
+        for nav in [navCharacter,navLocation, navEpisode] {
             nav.navigationBar.prefersLargeTitles = true
         }
         
         setViewControllers(
-            [navCharacter, navLocation, navEpisode, /*navSettings*/],
+            [navCharacter, navLocation, navEpisode],
             animated : true
         )
         
